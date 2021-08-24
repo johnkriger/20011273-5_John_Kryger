190d78520a6a60a4e2cdfeaa8518cc38a7372357
johnkriger <89279032+johnkriger@users.noreply.github.com>
2021-08-23 21:02:42 -0300

    Add files via upload

---
+++ 20011273-5_John_Kryger/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Alô Doçura</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="quem_somos.php">Quem somos?</a></li>
+                <li><a href="produtos.php">Produtos</a></li>
+            </ul>
+        </nav>
+    </header>
+    <footer>
+    <p>John Emerson Lopes Kryger</p>
+    <p>Login: 20011273-5</p>
+    <p>Curso: Superior de Tecnologia em Sistemas Para Internet</p>
+    </footer>
+    
+</body>
+</html>
